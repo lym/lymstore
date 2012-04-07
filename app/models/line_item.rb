bb4fa9fc9c6e21de819409e29d9ec9bd8770a494
salym <salymsash@gmail.com>
2012-04-08 01:43:12 +0300

    added delete functionality to cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,8 @@
 class LineItem < ActiveRecord::Base
 	belongs_to :cart
 	belongs_to :product
+
+	def total_price
+		product.price * quantity
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120407004335) do
+ActiveRecord::Schema.define(:version => 20120407164009) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20120407004335) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "products", :force => true do |t|
